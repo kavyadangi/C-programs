323270b6fd42310be8ee5f12e99ca8d8d3a98718
Kavya <kavyadangi09@gmail.com>
2023-10-19 15:51:41 +0530

    Create courserapoem.c

---
+++ courserapoem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("Underneath my outside face\nThere's a face that none can see.\nA little less smiley,\nA little less sure,\nBut a whole lot more like me");
+return 0;
+}
+
+
+
+
